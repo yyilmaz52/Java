dfd9d7a51cc8b0db0ae7eec666bda9d41d7240ea
Yusuf Yılmaz <81640286+yyilmaz52@users.noreply.github.com>
2022-06-21 11:33:39 +0300

    Add files via upload

---
+++ Composition/Bed.java
@@ -0,0 +1,42 @@
+
+public class Bed {
+
+	private String style;
+	private int pillows;
+	private int height;
+	private int sheets;
+	private int quilt;
+
+	public Bed(String style,  int pillows , int height, int sheets, int quilt) {
+		
+		this.style = style;
+		this.pillows = pillows;
+		this.height = height;
+		this.sheets = sheets;
+		this.quilt = quilt ; 
+		
+	}
+
+
+
+	public void make() {
+		System.out.println("Bed -> Making | ");
+	}
+
+	public String getStyle() {
+		return style;
+	}
+	public int getPillows () {
+		return pillows;
+	}
+	public int height() {
+		return height;
+	}
+	public int sheets () {
+		return sheets;
+	}
+	public int quilt () {
+		return quilt;
+	}
+
+}
---
+++ Composition/Wall.java
@@ -0,0 +1,17 @@
+
+public class Wall {
+
+	private String direction;
+	
+	
+	public Wall(String direction) {
+		this.direction = direction;
+	}
+
+
+	public String getDirection() {
+		return direction;
+	}
+	
+	
+}
